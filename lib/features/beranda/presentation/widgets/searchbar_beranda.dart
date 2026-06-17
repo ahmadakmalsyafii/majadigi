d824c8805240610641bed093720a71c698ea232a
ahmadakmalsyafii <barakba12344@gmail.com>
2026-06-17 11:38:50 +0700

    feat: enhance search bar functionality with navigation to layanan page

--- lib/features/beranda/presentation/widgets/searchbar_beranda.dart
+++ lib/features/beranda/presentation/widgets/searchbar_beranda.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchBarBeranda extends StatelessWidget {
   const SearchBarBeranda({super.key});
@@ -6,21 +7,25 @@ class SearchBarBeranda extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsetsGeometry.all(24.0),
-      child: SearchBar(
-        hintText: 'Cari Layanan dan Informasi...',
-        padding: WidgetStatePropertyAll(
-          const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-        ),
-        textStyle: WidgetStatePropertyAll(TextStyle(color: Colors.white)),
-        surfaceTintColor: WidgetStatePropertyAll(Colors.white),
-        shadowColor: WidgetStatePropertyAll(Colors.transparent),
-        side: WidgetStatePropertyAll(BorderSide(color: Colors.white)),
-        leading: Icon(Icons.search_rounded, color: Colors.white),
-        backgroundColor: WidgetStatePropertyAll(Colors.transparent),
-        onChanged: (value) {
-          // Implementasi pencarian di sini
+      padding: const EdgeInsets.all(24.0),
+      child: GestureDetector(
+        onTap: () {
+          context.go('/layanan');
         },
+        child: AbsorbPointer(
+          child: SearchBar(
+            hintText: 'Cari Layanan dan Informasi...',
+            padding: const WidgetStatePropertyAll(
+              EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            ),
+            textStyle: const WidgetStatePropertyAll(TextStyle(color: Colors.white)),
+            surfaceTintColor: const WidgetStatePropertyAll(Colors.white),
+            shadowColor: const WidgetStatePropertyAll(Colors.transparent),
+            side: const WidgetStatePropertyAll(BorderSide(color: Colors.white)),
+            leading: const Icon(Icons.search_rounded, color: Colors.white),
+            backgroundColor: const WidgetStatePropertyAll(Colors.transparent),
+          ),
+        ),
       ),
     );
   }
